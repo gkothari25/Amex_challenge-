e7a5d2a3c6a742403871f2fcaddb5075dcee0a32
Gaurav Kothyari <gkothari25@gmail.com>
2019-10-05 21:24:12 +0530

    latest_error

--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 import pandas as pd
 demogra_df = pd.read_csv('customer_demo_df_1.csv')
 c_trans_df = pd.read_csv('customer_transaction_data.csv')
-df1 = pd.merge(demogra_df, c_trans_df, how = 'inner', on = 'customer_id')
+df1 = pd.merge(demogra_df,c_trans_df, on = 'customer_id')
 print(df1.columns)
 
 
@@ -14,7 +14,7 @@ print(df1.columns)
 #lets merge train data and campaign data
 t_df = pd.read_csv('train.csv')
 c_df = pd.read_csv('campaign_data_df.csv')
-df2 = pd.merge(t_df, c_df, how = 'inner', on = 'campaign_id')
+df2 = t_df.join(c_df, on = 'campaign_id')
 print(df2.columns)
 
 
@@ -22,7 +22,7 @@ print(df2.columns)
 
 
 #lets combine df1 and df2 on custmor id
-df3 = pd.merge(t_df,df1,how='inner',on='customer_id')
+df3 = df2.merge(df1,on='customer_id')
 df3.to_csv('df3.csv')
 print("Done")
 																																																																																																																																																																																																																		
